1f2c9ce66ab0aed1237cb204e16e486b1fcf9ce8
Abdul Kalam <132799908+Kalam2002@users.noreply.github.com>
2024-09-06 20:40:19 +0530

    app.py

--- cryptex/app.py
+++ cryptex/app.py
@@ -1,38 +1,7 @@
 from flask import Flask, request, render_template, jsonify
-import joblib
-import numpy as np
-import math
-from collections import Counter
-from Crypto.Cipher import AES, DES, Blowfish
-from Crypto.Util.Padding import pad
-from Crypto.Random import get_random_bytes
-import hashlib
-import base64
-import rsa
+from main import predict_algorithm
 
 app = Flask(__name__)
-
-def calculate_entropy(text):
-    prob = [float(text.count(c)) / len(text) for c in dict.fromkeys(list(text))]
-    entropy = -sum([p * math.log2(p) for p in prob])
-    return entropy
-
-def extract_features(ciphertext):
-    counter = Counter(ciphertext)
-    char_freq = [counter.get(chr(i), 0) for i in range(256)]  # ASCII frequencies
-    char_freq = np.array(char_freq) / len(ciphertext)  # Normalize by length
-    entropy = calculate_entropy(ciphertext)
-    features = np.append(char_freq, entropy)
-    return features
-
-def predict_algorithm(ciphertext):
-    model = joblib.load('crypto_classifier_model.pkl')
-    scaler = joblib.load('scaler.pkl')
-    features = extract_features(ciphertext)
-    features_scaled = scaler.transform([features])
-    predicted_algo = model.predict(features_scaled)
-    algo_dict = {0: 'AES', 1: 'DES', 2: 'Blowfish', 3: 'RSA', 4: 'SHA-256', 5: 'MD5', 6: 'Caesar Cipher'}
-    return algo_dict.get(predicted_algo[0], 'Unknown')
 
 @app.route('/')
 def index():
